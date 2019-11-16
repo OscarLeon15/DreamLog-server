fada9e20119eee4af6521328ca52c18295dcc876
oscar leon <oscar.leon.delmar@gmail.com>
2019-11-16 13:54:39 -0600

    cloudinary fixed backend

--- routes/testing.js
+++ routes/testing.js
@@ -4,6 +4,7 @@ const uploadCloud = require('../configs/cloudinary/cloudinaryConfig')
 
 
 router.post('/testing', uploadCloud.single('theImage'), (req, res, next) => {
+  console.log(req.file )
   res.json(req.file.secure_url)
 });
 
