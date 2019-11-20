4e4cf91bbb3fc01b31d0f21aeda398d56873351f
Dusty <dustygaron@hotmail.com>
2019-11-20 09:28:16 -0500

    got routes working correctly.

--- configs/database/db.setup.js
+++ configs/database/db.setup.js
@@ -3,16 +3,7 @@ const mongoose = require("mongoose");
 mongoose.promise = Promise;
 
 mongoose
-  .connect('mongodb://localhost/ptwd-server1', {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
+  .connect('mongodb://localhost/dreamLogServer', { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
 
-
-  // mongoose
-//   .connect('mongodb://localhost/ptwb-server1', {useNewUrlParser: true})
-//   .then(x => {
-//     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-//   })
-//   .catch(err => {
-//     console.error('Error connecting to mongo', err)
-//   });
---
+++ models/dreamEntry.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const dreamEntrySchema = new Schema({
+  dreamText: {
+    type: String,
+    // required: false
+  }
+},
+  {
+    timestamps: true
+  });
+
+
+const dreamEntry = mongoose.model("dreamEntry", dreamEntrySchema);
+module.exports = dreamEntry;
+
+
+
+
+
