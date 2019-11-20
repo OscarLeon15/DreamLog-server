1d47c0df7f4571133ef95e540072963219f616a5
oscar leon <oscar.leon.delmar@gmail.com>
2019-11-20 11:17:24 -0600

    comment backend

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,29 +6,30 @@ const User = require('../models/User');
 
 
 router.post('/dashboard', (req, res, next) => {
-    const  parsedText  = req.body.savedText
-    console.log("req.body stuff", req.body)
+  const  parsedText  = req.body.savedText
+  // const  parsedText  = req.body.dreamText
+  console.log("req.body stuff", req.body)
     Dashboard.create({
       parsedText: parsedText
     })
     .then(themWords => {
       console.log(themWords, "the thing words");
-      console.log(parsedText, "the parsedtext words");
+      console.log("this is it!!!!! ---->: ", parsedText, " :<-----the parsedtext words");
   })
   .catch(err => console.log("Err in signup: ", err))
 });
 
-router.post('/personal', (req, res, next) => {
-    const  nameUser  = req.body.currentUser
+// router.post('/personal', (req, res, next) => {
+//     const  nameUser  = req.body.currentUser
     
-    Personal.create({
-      nameUser: nameUser
-    })
-    .then(themWords => {
-      console.log(themWords, " :the name");
-  })
-  .catch(err => console.log("Err in signup: ", err))
-});
+//     Personal.create({
+//       nameUser: nameUser
+//     })
+//     .then(themWords => {
+//       console.log(themWords, " :the name");
+//   })
+//   .catch(err => console.log("Err in signup: ", err))
+// });
 
 
 
